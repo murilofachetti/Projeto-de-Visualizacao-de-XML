3c442ef634019a28a3d6e9c4694b495df8499d79
Murilo Fachetti <110673100+murilofachetti@users.noreply.github.com>
2023-07-26 09:23:24 -0300

    Atualização
    Algumas alterações feitas no código. Falta agora implementação da funcionalidade de leitura de código de barras com o preenchimento de confirmação de leitura em cada linha correspondente ao código lido.

--- script.js
+++ script.js
@@ -1,66 +1,83 @@
-  $(document).ready(function() {
-    // Captura o evento de seleção de arquivo
-    $('#fileInput').on('change', function(e) {
-      let file = e.target.files[0];
-      let reader = new FileReader();
+$(document).ready(function () {
+  let xmlData; // Variável para armazenar os dados do XML após a leitura
 
-      reader.onload = function(e) {
-        let xmlContent = e.target.result;
-        let xmlDoc = $.parseXML(xmlContent);
-        let $xml = $(xmlDoc);
+  // Função para exibir a tabela e marcar a linha correspondente ao código de barras lido
+  function showTableAndHighlightRow(codigoLido) {
+    if (!xmlData) return; // Se os dados do XML ainda não foram lidos, sair da função
+    $("#xmlTable").empty(); // Limpa a tabela para inserir novos dados
 
-        // Extraia as informações desejadas do XML e crie a tabela
-        let clientName = $xml.find('CutList').find('ClientNest').text();
-        $('#clientName').text('Cliente: ' + clientName);
-        let ambiente = $xml.find('CutList').find('StackingLayout').text();
-        $('#ambiente').text('Ambiente:' + ambiente)
+    let tableContent =
+      "<tr><th>ID</th><th>Peça</th><th>Modulo</th><th>Largura</th><th>Altura</th><th>Cor</th><th>Código</th></tr>";
 
-        let tableContent = '';
-        $xml.find('Part').each(function() {
-          let id = $(this).attr('id');
-          let peca = $(this).attr('CabDesc');
-          let larg = $(this).attr('L');
-          let alt = $(this).attr('W');
-          let cor = $(this).attr('Material');
-          let codigo = $(this).attr('MatEdgeUp');
-          tableContent += '<tr><td>' + id + '</td><td>' + peca + '</td><td>' + larg + '</td><td>' + alt + '</td><td>' + cor + '</td><td>' + codigo + '</td></tr>';
-        });
+    // Loop para percorrer os dados do XML
+    xmlData.find("Part").each(function () {
+      let id = $(this).attr("id");
+      let peca = $(this).attr("CabDesc");
+      let modulo = $(this).attr("CabCode");
+      let larg = $(this).attr("L");
+      let alt = $(this).attr("W");
+      let cor = $(this).attr("Material");
+      let codigo = $(this).attr("MatEdgeUp");
 
-        $('#xmlTable').html('<tr><th>ID</th><th>Peça</th><th>Largura</th><th>Altura</th><th>Cor</th><th>Código</th></tr>' + tableContent);
+      // Verifica se o código lido corresponde ao código da peça atual no loop
+      let isCodigoLido = codigoLido === codigo;
 
-        // Exibe a tabela
-        $('#tableContainer').addClass('show');
-      };
+      // Se o código foi lido, adiciona a classe "highlight" para destacar a linha
+      let rowClass = isCodigoLido ? "highlight" : "";
 
-      reader.readAsText(file);
+      // Constrói a linha da tabela com os dados da peça e a classe de destaque, se aplicável
+      tableContent += `<tr class="${rowClass}">
+          <td>${id}</td>
+          <td>${peca}</td>
+          <td>${modulo}</td>
+          <td>${larg}</td>
+          <td>${alt}</td>
+          <td>${cor}</td>
+          <td>${codigo}</td>
+        </tr>`;
     });
+
+    // Insere o conteúdo da tabela na página
+    $("#xmlTable").html(tableContent);
+
+    // Exibe a tabela
+    $("#tableContainer").show();
+  }
+
+  // Captura o evento de seleção de arquivo
+  $("#fileInput").on("change", function (e) {
+    let file = e.target.files[0];
+    let reader = new FileReader();
+
+    reader.onload = function (e) {
+      let xmlContent = e.target.result;
+      let xmlDoc = $.parseXML(xmlContent);
+      xmlData = $(xmlDoc);
+
+      // Extrair o nome do cliente do XML
+      const clientName = xmlData.find("Part").eq(0).attr("ClientNest");
+
+      // Exibir o nome do cliente na área designada
+      $("#clientName").text("CLIENTE: " + clientName);
+
+      // Extrair o nome do ambiente do XML
+      const ambiente = xmlData.find("Part").eq(0).attr("StackingLayout");
+
+      // Exibir o nome do ambiente na área designada
+      $("#ambiente").text("AMBIENTE: " + ambiente);
+
+      showTableAndHighlightRow(""); // Exibe a tabela vazia ao carregar o XML
+    };
+
+    reader.readAsText(file);
   });
 
-// Download dos Arquivos em forma de Planilha.
+  // Simulação do evento de leitura do código de barras
+  // Supondo que você tenha um leitor que chama essa função quando lê um código de barras
+  function onBarcodeRead(codigoLido) {
+    showTableAndHighlightRow(codigoLido);
+  }
 
-// const downloadXLSX = () => {
-//     const wb = XLSX.utils.book_new();
-
-//     wb.Props = {
-//         Title: 'Relatório',
-//         Assunto: '...',
-//     };
-
-//     wb.SheetNames.push('Relatório 01');
-
-//     const dados = [
-//         ['Nome', 'Usuário'],
-//         ['teste', 'teste']
-//     ];
-
-//     const ws = XLSX.utils.aoa_to_sheet(dados);
-
-//     wb.Sheets['Relatório 01'] = ws;
-
-//     XLSX.writeFile(wb, 'Relatório do Projeto.xlsx', { bookType: 'xlsx', type: 'bynary'})};
-
-// document.getElementById('download').addEventListener('click', () => {
-//     downloadXLSX();
-// });
-
-// Download dos Arquivos em forma de Planilha.
+  // Exemplo: chamando onBarcodeRead com o código "12345" quando um código de barras é lido
+  onBarcodeRead("12345");
+});
